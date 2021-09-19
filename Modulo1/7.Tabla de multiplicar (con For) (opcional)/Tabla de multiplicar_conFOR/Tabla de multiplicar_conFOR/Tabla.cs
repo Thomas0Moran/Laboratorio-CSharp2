d29e0a34e717e01708a1bebf3c781ae171f370f1
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-19 20:55:10 -0300

    se realizo el ejercicio 7 del modulo 1

---
+++ Modulo1/7.Tabla de multiplicar (con For) (opcional)/Tabla de multiplicar_conFOR/Tabla de multiplicar_conFOR/Tabla.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_multiplicar_conFOR
+{
+    class Tabla
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Tabla de multiplicar (con for)---");
+            Console.WriteLine();
+
+            int NUM, I, RESUL;
+            string linea;
+
+            Console.WriteLine("Seleccione el numero que desea multiplicar: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            Console.WriteLine("");
+
+            for(I = 1; I <= 12; I++)
+            {
+                RESUL = NUM * I;
+                Console.WriteLine($"{NUM} * {I} = {RESUL}");
+            }
+            Console.WriteLine("Presione una tecla para cerrar.");
+            Console.ReadKey();
+
+        }
+    }
+}
