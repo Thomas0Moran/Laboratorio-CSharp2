2b8cdc683d4a90b4fa570632c9c805c35e073468
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-19 17:53:36 -0300

    Se realizo el ejercicio 2 del mudulo 1 Compra de Restaurant

---
+++ Modulo1/2.Compra en restaurant/Compra de restaurant/Compra de restaurant/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compra_de_restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Compra de restaurant---");
+            Console.WriteLine();
+            byte CantHamburguesa, CantPapas, CantGaseosa;
+            double Apagar;
+            string linea;
+
+            const double PrecioH = 2;
+            const double PrecioP = 1;
+            const double PrecioG = 1.5;
+
+            Console.WriteLine($"Cantidad de Hamburguesas: ");
+            linea = Console.ReadLine();
+            CantHamburguesa = byte.Parse(linea);
+            Console.WriteLine();
+
+            Console.WriteLine("Cantidad de Papas: ");
+            linea = Console.ReadLine();
+            CantPapas = byte.Parse(linea);
+
+            Console.WriteLine("Cantidad de Gaseosas: ");
+            linea = Console.ReadLine();
+            CantGaseosa = byte.Parse(linea);
+
+            Apagar = (CantHamburguesa * PrecioH) + (CantPapas * PrecioP) + (CantGaseosa * PrecioG);
+            Console.WriteLine($"Cantidad a pagar: {Apagar}$");
+            Console.ReadKey();
+
+
+        }
+    }
+}
