29a000fbdc8806a0c463419cbfb8a1f759e61aec
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-19 17:34:01 -0300

    se resolvio el ejercicio numero 1 del modulo 1 OPERACIONES BASICAS

---
+++ Modulo1/1.Operaciones básicas/Operaciones Basicas/Operaciones Basicas/OpBasicas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones_Basicas
+{
+    class OpBasicas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Operaciones basicas---");
+            int NUM1, NUM2, RESUL;
+            string linea;
+
+
+            Console.WriteLine("Seleccione el primer numero:");
+            linea = Console.ReadLine();
+            NUM1 = int.Parse(linea);
+            Console.WriteLine();
+
+            Console.WriteLine("Seleccione el segundo numero:");
+            linea = Console.ReadLine();
+            NUM2 = int.Parse(linea);
+            Console.WriteLine();
+
+            RESUL = NUM1 + NUM2;
+            Console.WriteLine($"La suma de {NUM1} y {NUM2} da un total de={RESUL}");
+
+            RESUL = NUM1 - NUM2;
+            Console.WriteLine($"La resta de {NUM1} y {NUM2} da un total de={RESUL}");
+
+            RESUL = NUM1 * NUM2;
+            Console.WriteLine($"La multipicación de {NUM1} y {NUM2} da un total de={RESUL}");
+
+            RESUL = NUM1 / NUM2;
+            Console.WriteLine($"La división de {NUM1} y {NUM2} da un total de={RESUL}");
+
+            RESUL = NUM1 % NUM2;
+            Console.WriteLine($"El reciduo de {NUM1} y {NUM2} da un total de={RESUL}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
