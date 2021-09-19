29b9f6df4ef6de602f005e760a6a1d3293a64dad
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-19 20:27:22 -0300

    Se realizo el ejercicio 5 del modulo 1 + se emprolijo el nombre de la clase del ejercicio 4

--- Modulo1/4.Desglose de billetes/Desglose de billetes/Desglose de billetes/Dbilletes.cs
+++ Modulo1/4.Desglose de billetes/Desglose de billetes/Desglose de billetes/Dbilletes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Desglose_de_billetes
 {
-    class Program
+    class Dbilletes
     {
         static void Main(string[] args)
         {
@@ -24,27 +24,27 @@ namespace Desglose_de_billetes
             linea = Console.ReadLine();
             CAN = int.Parse(linea);
 
-            //if ((CAN >= 100))
-            //{
-            //    C100 = (CAN / 100);
-            //    CAN = CAN - (C100 * 100);
-            //}
-            //if ((CAN >= 50))
-            //{
-            //    C50 = (CAN / 50);
-            //    CAN = CAN - (C50 * 50);
-            //}
-            //if((CAN >= 20))
-            //{
-            //    C20 = (CAN / 20);
-            //    CAN = CAN - (C20 * 20);
-            //}
-            //if((CAN >= 10))
-            //{
-            //    C10 = (CAN / 10);
-            //    CAN = CAN - (C10 * 10);
-            //}
-            if((CAN >= 5))
+            if ((CAN >= 100))
+            {
+                C100 = (CAN / 100);
+                CAN = CAN - (C100 * 100);
+            }
+            if ((CAN >= 50))
+            {
+                C50 = (CAN / 50);
+                CAN = CAN - (C50 * 50);
+            }
+            if ((CAN >= 20))
+            {
+                C20 = (CAN / 20);
+                CAN = CAN - (C20 * 20);
+            }
+            if ((CAN >= 10))
+            {
+                C10 = (CAN / 10);
+                CAN = CAN - (C10 * 10);
+            }
+            if ((CAN >= 5))
             {
                 C5 = (CAN / 5);
                 CAN = C5 - (C5 * 5); 
---
+++ Modulo1/5.Triángulos (opcional)/Triángulos/Triángulos/Triangulos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triángulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---TRIÁNGULOS---");
+            Console.WriteLine();
+
+            int LADO1, LADO2, LADO3;
+            string linea;
+
+            Console.Write("Seleccione el primer lado del triángulo: ");
+            linea = Console.ReadLine();
+            LADO1 = Convert.ToInt16(linea);
+            Console.WriteLine();
+
+            Console.Write("Seleccione el segundo lado del triángulo: ");
+            linea = Console.ReadLine();
+            LADO2 = Convert.ToInt16(linea);
+            Console.WriteLine();
+
+            Console.Write("Seleccione el tercer lado del triangulo: ");
+            linea = Console.ReadLine();
+            LADO3 = Convert.ToInt16(linea);
+            Console.WriteLine();
+
+            if ((LADO1 == LADO2) & (LADO1 == LADO3))
+            {
+                Console.WriteLine("Los lados formaran un triangulo Equilatero");//lados iguales
+            }
+            else
+            {
+                if ((LADO1 != LADO2) & (LADO1 != LADO3) & (LADO2 != LADO3))
+                {
+                    Console.WriteLine("Los lados formaran un triangulo Escaleno");//lados distintos
+                }
+                else
+                {
+                    Console.WriteLine("Los lados formaran un triangulo Isóseles");//solo dos lados iguales
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
