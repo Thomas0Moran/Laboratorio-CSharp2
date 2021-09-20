1cf1eaead9d5dbd1ea39316b3b96e2498137f15f
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-19 21:07:00 -0300

    Se realizo el ejercicio 8 del modulo 1

---
+++ Modulo1/8.Tabla de multiplicar (con While)/Tabla de multiplicar_con WHILE/Tabla de multiplicar_con WHILE/TablaWhile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_multiplicar_con_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Tabla de multiplicar con WHILE---");
+            Console.WriteLine();
+
+            int NUM, I, RESUL;
+            string linea;
+
+            Console.Write("Seleccione el numero que desea multiplicar: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            Console.WriteLine();
+
+            I = 1;
+            while((I <= 12))
+            {
+                RESUL = NUM * I;
+                Console.WriteLine($"{NUM} * {I} = {RESUL}");
+                I = I + 1;
+            }
+            Console.WriteLine("Presione cualquier tecla para cerrar.");
+
+            Console.ReadKey();
+        }
+    }
+}
