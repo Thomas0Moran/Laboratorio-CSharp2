590058b59a6a0132f06c0be89da13f29a2d10694
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-20 18:46:22 -0300

    Se realizo el ejercicio 11 del modulo 1

---
+++ Modulo1/12.Arreglo/Arreglo/Arreglo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Arreglo-----");
+            Console.WriteLine();
+
+            string[] VECTOR = { "LORENA", "ESTEBAN", "ARIEL" };
+            byte K;
+
+            for (K = 0; K <= 2; K++)
+            {
+                Console.WriteLine(VECTOR[K]);
+            }
+
+            Console.WriteLine("--------------------------------");
+
+            int[] MILISTA = null;
+            MILISTA = new int[] { 15, 20, 30, 10, 50, 60, 10, 150, 69 };
+
+            for (K = 0; K <= MILISTA.Length - 1; K++)
+            {
+                Console.WriteLine(MILISTA[K]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pulse una tecla...");
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
