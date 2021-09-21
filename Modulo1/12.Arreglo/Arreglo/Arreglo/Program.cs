38e1098a06908fc6b3f4aaf93a9076d1bfabadd0
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-21 18:49:19 -0300

    Se volvio a realizar el Ejercicio 12 del modulo 1 a modo de repaso + se realizaron experimentos

--- Modulo1/12.Arreglo/Arreglo/Arreglo/Program.cs
+++ Modulo1/12.Arreglo/Arreglo/Arreglo/Program.cs
@@ -10,28 +10,71 @@ namespace Arreglo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-----Arreglo-----");
-            Console.WriteLine();
+            Console.WriteLine("----Arreglos----");
+            Console.WriteLine("A)");
 
-            string[] VECTOR = { "LORENA", "ESTEBAN", "ARIEL" };
+            string[] VECTOR = { "THOMAS", "DAI", "DANIEL" };
+            string linea;
             byte K;
 
-            for (K = 0; K <= 2; K++)
+            for (K = 0; K <= 2; )
             {
                 Console.WriteLine(VECTOR[K]);
             }
+            Console.WriteLine("-----------------------------------------");
 
-            Console.WriteLine("--------------------------------");
-
+            Console.WriteLine("B)");
             int[] MILISTA = null;
-            MILISTA = new int[] { 15, 20, 30, 10, 50, 60, 10, 150, 69 };
-
+            MILISTA = new int[] {15, 20, 30, 10, 50, 60, 10, 150, 69};
             for (K = 0; K <= MILISTA.Length - 1; K++)
             {
                 Console.WriteLine(MILISTA[K]);
             }
             Console.WriteLine();
-            Console.WriteLine("Pulse una tecla...");
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("----Ejercicos varios 1-3---- ");
+            
+            int[] LosValores = new int[4];
+            LosValores[0] = 154;
+            LosValores[1] = 10;
+            LosValores[2] = -1864;
+            LosValores[3] = 0;
+
+            for(int i = 0; i < LosValores.Length; i++)
+            {
+                Console.WriteLine(LosValores[i]);
+            }
+
+            Console.WriteLine("----Ejercicos varios 2-3---- ");
+            int[] Test = new int[6];
+            Test[0] = 1;
+            Test[1] = 2;
+            Test[2] = 3;
+            Test[3] = 4;
+            Test[4] = 5;
+            Test[5] = 6;
+            for(int F = 0; F < Test.Length; F++)
+            {
+                Console.WriteLine(Test[F]);
+            }
+
+            Console.WriteLine("----Ejercicos varios 3-3---- ");
+            Console.WriteLine();
+            string[] Cosas = { "TIJERA", "PIEDRA", "PAPEL", };
+            string Line;
+            int B;
+
+            Console.WriteLine("Oprima cualquier tecla para ver el contenido: ");
+            Line = Console.ReadLine();
+            B = int.Parse(Line);
+
+            for( B = 0; B <=2; B++)
+            {
+                Console.WriteLine(Cosas[B]);
+            }
+            Console.WriteLine();
 
 
 
